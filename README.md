b4a0c318f0e569b34af0267342b04671d941b0f3
vmiis <zzm.vmiis@gmail.com>
2016-02-24 13:43:59 +1100

    update

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Car-Booking
+<a target=_blank href=https://cbs.wappsystem.com/dev/github/wappsystem/Car-Booking/index.html>Hosted on Wappsystem</a><br>
+<a target=_blank href=https://htmlpreview.github.io/?https://raw.githubusercontent.com/wappsystem/Car-Booking/master/index.html>Hosted on GitHub</a>
+<img src=https://cbs.wappsystem.com/dev/github/vmiis/SaaS/images/structure.png?v=2 />
--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 $vm.module_list={
-    layout:                      ['--------','.../layout.html',''],
-    home:                        ['--------','.../modules/home.html',''],
-    navigation:                  ['--------','.../modules/navigation.html',''],
-    daily:                       ['--------','.A./operation/car_booking/daily.html','',  'booking_record'],
-    weekly:                      ['--------','.A./operation/car_booking/weekly.html','', 'booking_record'],
-    monthly:                     ['--------','.A./operation/car_booking/monthly.html','','booking_record'],
-    car:                         ['20003816','.A./operation/car_booking/car.html',''],
-    booking_record:              ['20003817','.A./operation/car_booking/booking_record.html',''],
-    upload_file:                 ['20007156','.A./document/upload_file.html',''],
+    layout:                      ['--------','.../layout.html','2'],
+    home:                        ['--------','.../modules/home.html','2'],
+    navigation:                  ['--------','.../modules/navigation.html','2'],
+    daily:                       ['--------','.A./operation/car_booking/daily.html','2',  'booking_record'],
+    weekly:                      ['--------','.A./operation/car_booking/weekly.html','2', 'booking_record','daily'],
+    monthly:                     ['--------','.A./operation/car_booking/monthly.html','2','booking_record','daily'],
+    car:                         ['20003816','.A./operation/car_booking/car.html','2'],
+    booking_record:              ['20003817','.A./operation/car_booking/booking_record.html','2'],
+    upload_file:                 ['20007156','.A./document/upload_file.html','2'],
 }
 //--------------------------------------------------------
 for(key in $vm.module_list){
-    //$vm.module_list[key].push($vm.module_list[key][1]);
-    $vm.module_list[key][1]=$vm.module_list[key][1].replace('...','__BASE__/'+$vm.repository)
-    $vm.module_list[key][2]=$vm.module_list[key][2].replace('...','https://github.com/'+$vm.repository+'/blob/master')
-    $vm.module_list[key][1]=$vm.module_list[key][1].replace('.A.','__BASE__/wappsystem/Common-Modules')
-    $vm.module_list[key][2]=$vm.module_list[key][2].replace('.A.','https://github.com/wappsystem/Common-Modules/blob/master')
+    $vm.module_list[key][2]=$vm.module_list[key][1];
+    $vm.module_list[key][1]=$vm.url($vm.module_list[key][1].replace('...','__BASE__/'+$vm.repository));
+    $vm.module_list[key][2]=$vm.module_list[key][2].replace('...','https://github.com/'+$vm.repository+'/blob/master');
+    $vm.module_list[key][1]=$vm.url($vm.module_list[key][1].replace('.A.','__BASE__/wappsystem/Common-Modules'));
+    $vm.module_list[key][2]=$vm.module_list[key][2].replace('.A.','https://github.com/wappsystem/Common-Modules/blob/master');
 }
 //--------------------------------------------------------
 var jsN=0;
@@ -27,6 +27,7 @@ var last=function(){
         $.getScript('https://ajax.aspnetcdn.com/ajax/jquery.validate/1.14.0/jquery.validate.min.js',function(){last_last();});
         $.getScript('https://sdk.amazonaws.com/js/aws-sdk-2.1.34.min.js',function(){last_last();});
         $.getScript('https://cbs.wappsystem.com/system/third/handsontable.full.min.js',function(){last_last();});
+        $.getScript('https://cbs.wappsystem.com/system/third/ace/ace.js',function(){last_last();});
         $.getScript('https://cbs.wappsystem.com/dev/vm.js?v=20160108',function(){last_last();});
         $.getScript('https://www.google.com/jsapi',function(){
             google.load('visualization', '1', {'packages':['corechart'],callback:function(){last_last();} } );
@@ -38,6 +39,6 @@ var last=function(){
 }
 //--------------------------------------------------------
 var last_last=function(){
-    jsN++; if(jsN===6) $vm.load_demo();
+    jsN++; if(jsN===7) $vm.load_demo();
 }
 //--------------------------------------------------------
